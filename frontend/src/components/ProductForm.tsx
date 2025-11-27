417fe56a9d479eb6dbfd91c1f473ff0871a83cec
m <m@mail.com>
2025-11-27 08:02:57 +0100

    display template fields required

--- frontend/src/components/ProductForm.tsx
+++ frontend/src/components/ProductForm.tsx
@@ -1,4 +1,7 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
+import { Input } from './ui/input';
+import { Select } from './ui/select';
+import { Label } from './ui/label';
 
 type Product = {
   id: string;
@@ -17,6 +20,7 @@ type Field = {
   id: string;
   label: string;
   type: FieldType;
+  isRequired: boolean; // Required if set to true or if the field is comming from a template
   created: string;
   updated: string;
 }
@@ -29,6 +33,12 @@ type ProductField = {
   fieldId: Field['id'];
   created: string;
   updated: string;
+}
+
+type FieldExpandWithProductField = Field & {
+  expand: {
+    productFields: ProductField[];
+  }
 }
 
 type ProductStock = {
@@ -48,10 +58,16 @@ export function ProductForm({ productId, templateId }) {
   const [product, setProduct] = useState<Product | null>(null);
   const [productTemplateFields, setProductTemplateFields] = useState<ProductField[]>([]);
   const [productFields, setProductFields] = useState<Field[]>([]);
+  const productFieldsExpanded = useMemo(() => {
+    return matchProductFieldsToFields(productFields, productTemplateFields);
+  }, [productFields, productTemplateFields]);
   //TEMPLATE
   const [template, setTemplate] = useState<Product | null>(null);
   const [templateProductFields, setTemplateProductFields] = useState<ProductField[]>([]);
   const [templateFields, setTemplateFields] = useState<Field[]>([]);
+  const templateFieldsExpanded = useMemo(() => {
+    return matchProductFieldsToFields(templateFields, templateProductFields);
+  }, [templateFields, templateProductFields]);
 
   async function getProductById(productId: Product['id']): Promise<Product> {
     return {
@@ -67,6 +83,7 @@ export function ProductForm({ productId, templateId }) {
         id: '1',
         label: 'Prix',
         type: FieldType.TEXT,
+        isRequired: true,
         created: '2021-01-01',
         updated: '2021-01-01',
       },
@@ -74,6 +91,7 @@ export function ProductForm({ productId, templateId }) {
         id: '2',
         label: 'Images',
         type: FieldType.IMAGES,
+        isRequired: false,
         created: '2021-01-01',
         updated: '2021-01-01',
       }
@@ -101,6 +119,21 @@ export function ProductForm({ productId, templateId }) {
         updated: '2021-01-01',
       }
     ]
+  }
+
+  function matchProductFieldsToFields(Fields: Field[], ProductFields: ProductField[]): FieldExpandWithProductField[] {
+    
+    return Fields.map(field => {
+
+      const productFields = ProductFields.filter(productField => productField.fieldId === field.id);
+
+      return { 
+        ...field, 
+        expand: { 
+          productFields: productFields 
+        } 
+      };
+    });
   }
 
   async function init() {
@@ -148,20 +181,98 @@ export function ProductForm({ productId, templateId }) {
       <div>
         <h2>Template</h2>
         <pre>{JSON.stringify(template, null, 2)}</pre>
-        <p>Template Product Fields</p>
-        <pre>{JSON.stringify(templateProductFields, null, 2)}</pre>
-        <p>Template Fields</p>
-        <pre>{JSON.stringify(templateFields, null, 2)}</pre>
-      </div>
 
+      </div>
 
       <div>
-        <h2>Product</h2>
-        <pre>{JSON.stringify(product, null, 2)}</pre>
-        <pre>{JSON.stringify(productTemplateFields, null, 2)}</pre>
-        <pre>{JSON.stringify(productFields, null, 2)}</pre>
+        <h2>Template Fields</h2>
+        {templateFieldsExpanded.map(field => (
+          <div key={field.id}>
+            {fieldToComponent(field)}
+            <h3>{field.label}</h3>
+            {field.expand.productFields.map(productField => (
+              <div key={productField.id}>{productField.value}</div>
+            ))}
+          </div>
+        ))}
       </div>
 
+    </div>
+  )
+}
+
+
+function fieldToComponent(field: FieldExpandWithProductField): React.ReactNode {
+  switch (field.type) {
+    case FieldType.TEXT:
+
+      const productField = field.expand.productFields[0];
+    
+      return <TextField field={field} value={productField.value} isRequired={field.isRequired} />;
+
+    case FieldType.NUMBER:
+      return <NumberField field={field} isRequired={field.isRequired} />;
+    case FieldType.SELECT:
+      return <SelectField field={field} isRequired={field.isRequired} />;
+    case FieldType.IMAGES:
+
+      const images = field.expand.productFields.map(productField => productField.images);
+
+      return <ImagesField field={field} images={images} isRequired={field.isRequired} />;
+    default:
+      return null;
+  }
+}
+
+function ImagesField({ field, images, isRequired }: { field: Field, images: string[], isRequired: boolean }) {
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const files = Array.from(e.target.files || []);
+  }
+
+  return (
+    <div>
+      <Label>{field.label} <span className="text-destructive" hidden={!isRequired}>*</span> </Label>
+      {images.map(image => (
+        <img src={image} alt={image} />
+      ))}
+      <input type="file" multiple onChange={handleChange} required={isRequired} />
+    </div>
+  )
+  
+}
+
+function TextField({ field, value, isRequired }: { field: Field, value?: string, isRequired: boolean }) {
+
+  const [fieldValue, setFieldValue] = useState(value || '');
+
+  return (
+    <div>
+      <Label>{field.label} <span className="text-destructive" hidden={!isRequired}>*</span> </Label>
+      <Input type="text" value={fieldValue} onChange={(e) => setFieldValue(e.target.value)} required={isRequired} />
+    </div>
+  )
+
+}
+
+function NumberField({ field, isRequired }: { field: Field, isRequired: boolean }) {
+  const [fieldValue, setFieldValue] = useState(0);
+
+  return (
+    <div>
+      <Label>{field.label} <span className="text-destructive" hidden={!isRequired}>*</span> </Label>
+      <Input type="number" value={fieldValue} onChange={(e) => setFieldValue(e.target.value)} required={isRequired} />
+    </div>
+  )
+}
+
+function SelectField({ field, isRequired }: { field: Field, isRequired: boolean }) {
+  const [value, setValue] = useState('');
+
+  return (
+    <div>
+      <Label>{field.label} <span className="text-destructive" hidden={!isRequired}>*</span> </Label>
+      <Select value={value} onValueChange={(value) => setValue(value)} required={isRequired} />
     </div>
   )
 }
